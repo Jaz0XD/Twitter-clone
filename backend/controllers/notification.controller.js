0114cbc8fbec0a7768b309dcf8431772066083dd
Jaz0XD <jazwastaken.xd@gmail.com>
2024-07-19 20:20:34 +0530

    1 notification delete option

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -46,3 +46,35 @@ export const deleteNotifications = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+//* DELETE ONE NOTIFICATION
+
+export const deleteNotification = async (req, res) => {
+  try {
+    const notificationId = req.params.id;
+    const userId = req.user._id;
+    const notification = await Notification.findById(notificationId);
+
+    if (!notification) {
+      return res.status(404).json({ error: "Notification not found" });
+    }
+
+    if (notification.to.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ error: "You are not allowed to delete this notification" });
+    }
+
+    await Notification.findByIdAndDelete(notificationId);
+    res.status(200).json({ message: "Notification deleted successfully" });
+  } catch (error) {
+    //* Log error in Console
+    console.log(
+      "[notification.controller.js] Error in deleteNotification controller: ",
+      error
+    );
+
+    //* Send error message to user
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
