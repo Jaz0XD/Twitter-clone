e203cf97a618c98040a1fcf91c2720e9701923b9
Jaz0XD <jazwastaken.xd@gmail.com>
2024-07-21 19:48:46 +0530

    like posts added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -132,7 +132,12 @@ export const likeUnlikePost = async (req, res) => {
       //* Unlike post
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
       await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-      res.status(200).json({ message: "Post unliked successfully" });
+
+      //* Removes user from the likes array
+      const updatedLikes = post.likes.filter(
+        (id) => id.toString() !== userId.toString()
+      );
+      res.status(200).json(updatedLikes);
     } else {
       //* Like post
       post.likes.push(userId);
@@ -146,7 +151,8 @@ export const likeUnlikePost = async (req, res) => {
       });
       await notification.save();
 
-      res.status(200).json({ message: "Post liked successfully" });
+      const updatedLikes = post.likes;
+      res.status(200).json(updatedLikes);
     }
   } catch (error) {
     //* Log error in Console
